a7ea9afd2413101a31dc6b41ca8ea84515724c35
Sagar Chawla <sagarchawla83@gmail.com>
2023-10-24 01:25:29 +0530

    done

--- src/scenes/CreateRoom/CreateRoom.js
+++ src/scenes/CreateRoom/CreateRoom.js
@@ -9,7 +9,7 @@ export const CreateRoom = ({isAuth, setIsAuth}) => {
         <Navbar isAuth={isAuth} setIsAuth={setIsAuth}/>
         <div className='create-room-body'>
             <div className='chat-room-description'>
-              <h1>Create Your Own Chatroom . . . . .</h1>
+              <h1 className='gradient-heading'>Create Your Own Chatroom . . . . .</h1>
               <p>Empower your community with our user-friendly "Create Your Chatroom" page. Simply fill out the form and launch your personalized chat space. Instant connection, seamless conversations—your chatroom, your rules!</p>
             </div>
             <div className="create-chat-room-form">
